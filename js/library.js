0c1feec0ce583dd2ee22b4b6c0f991206ee660d0
afzalhossain00 <afzalhossain.workspace@gmail.com>
2022-08-21 04:55:59 +0600

    added tax and final amount with function

--- js/library.js
+++ js/library.js
@@ -6,12 +6,24 @@ function getTextElementById(elementId) {
 
 }
 
+function setTextElementValueById(elementId, newValue) {
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = newValue;
+}
+
 function calculateSubTotal() {
     // calculate total
     const currentPhoneTotal = getTextElementById('phone-total');
     const currentCaseTotal = getTextElementById('case-total');
-
     const currentSubTotal = currentPhoneTotal + currentCaseTotal;
-    const subTotalElement = document.getElementById('sub-total');
-    subTotalElement.innerText = currentSubTotal;
+    setTextElementValueById('sub-total', currentSubTotal);
+
+    // calculae tax amount
+    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementValueById('tax-amount', taxAmount);
+
+    // calculate final amount 
+    const finalAmount = currentSubTotal + taxAmount;
+    setTextElementValueById('final-total', finalAmount);
 }
